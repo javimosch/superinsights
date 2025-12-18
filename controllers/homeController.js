e1aa5383142247f531556e114594164c71546108
Jarancibia <arancibiajav@gmail.com>
2025-12-18 17:33:16 +0100

    feat: Redirect authenticated users from home to projects

--- controllers/homeController.js
+++ controllers/homeController.js
@@ -1,5 +1,9 @@
 exports.getHome = (req, res, next) => {
   try {
+    if (req.session && req.session.user) {
+      return res.redirect('/projects');
+    }
+
     res.render('index', {
       title: 'SuperInsights',
       message: 'Welcome to the SuperInsights starter app',
